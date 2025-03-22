9f4fe28d473f9b1c1a432488437845f68d7dd414
Dilawar <dilawarshah0230@gmail.com>
2025-03-23 00:41:26 +0500

    added settings, leave, payroll pages with html

--- myapp/urls.py
+++ myapp/urls.py
@@ -7,6 +7,10 @@ urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
     path('logout/', views.logout_view, name='logout'),
     path('login/', views.login_view, name='login'),
+    path('payroll/', views.payroll_view, name='payroll'),
+    path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('leave/', views.leave_view, name='leave'),
+    path('settings/', views.settings_view, name='settings'),
 ]
 
 
--- myapp/views.py
+++ myapp/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, "index.html")  # Renders the HTML file
+    return render(request, "index.html")  # Renders the login page
 
 def login_view(request):
-    return render(request, "login.html")
+    return render(request, "login.html") # renders login page
 
 def logout_view(request):
-    return render(request, "login.html")
+    return render(request, "login.html") # renders login page
 
 def signup_view(request):
-    return render(request, "signup.html")
+    return render(request, "signup.html") # renders signup page
+
+def payroll_view(request):
+    return render(request, 'payroll.html')
+
+def dashboard_view(request):
+    return render(request, "index.html")
+
+def leave_view(request):
+    return render(request, "leave.html")
+
+def settings_view(request):
+    return render(request, "settings.html")
