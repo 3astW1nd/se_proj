55b81c9ec1fd0e4f8882f317f5a2e1c263324206
Dilawar <dilawarshah0230@gmail.com>
2025-03-22 19:38:45 +0500

    added basic html pages with css and changed the views.py and urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from .views import home
+from myapp import views
 
 urlpatterns = [
     path('', home, name='home'),
+    path('signup/', views.signup_view, name='signup'),
+    path('logout/', views.logout_view, name='logout'),
+    path('login/', views.login_view, name='login'),
 ]
+
+
--- myapp/views.py
+++ myapp/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "index.html")  # Renders the HTML file
+
+def login_view(request):
+    return render(request, "login.html")
+
+def logout_view(request):
+    return render(request, "login.html")
+
+def signup_view(request):
+    return render(request, "signup.html")
